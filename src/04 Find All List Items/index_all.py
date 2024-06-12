e48e4b6f761fde04c076133b900105f0d24cdcc7
jiliang <54050550+jiliang96@users.noreply.github.com>
2024-06-12 18:51:13 +0000

    Q4

--- src/04 Find All List Items/index_all.py
+++ src/04 Find All List Items/index_all.py
@@ -14,3 +14,5 @@ if __name__ == '__main__':
     example = [[[1, 2, 3], 2, [1, 3]], [1, 2, 3]]
     print(index_all(example, 2))  # [[0, 0, 1], [0, 1], [1, 1]]
     print(index_all(example, [1, 2, 3]))  # [[0, 0], [1]]
+
+    print(index_all(example, 4))
